0b4560875a03ac1bcf6251e893f79c6055e0185d
mattwills8 <matt_wills8@hotmail.co.uk>
2019-04-08 12:35:46 -0600

    override parent theme page templates with a simple message

---
+++ src/page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package torque
+ */
+?>
+<?php TQ::get_template_parts( array( 'parts/shared/html-header') ); ?>
+
+<main>
+	This site is solely for managing data and exposing an API.
+</main>
+
+<?php TQ::get_template_parts( array( 'parts/shared/html-footer' ) ); ?>
